3cce7942ec2788a37a28de1d7e62789721589423
Kurt Manninen <kmanninen@brainchip.com>
2025-11-09 07:49:43 -0800

    Download Akida 1 models.

--- get_models.sh
+++ get_models.sh
@@ -2,15 +2,18 @@ cd examples
 mkdir -p models
 cd models
 
-wget https://data.brainchip.com/models/AkidaV2/tenn_spatiotemporal/tenn_spatiotemporal_eye_buffer_i8_w8_a8.h5
-wget https://data.brainchip.com/models/AkidaV2/tenn_spatiotemporal/tenn_spatiotemporal_jester_buffer_i8_w8_a8.h5
-wget https://data.brainchip.com/models/AkidaV2/centernet/centernet_akidanet18_voc_384_i8_w8_a8.h5
+wget https://data.brainchip.com/models/AkidaV1/akidanet/akidanet_vww_iq8_wq4_aq4.h5
+wget https://data.brainchip.com/models/AkidaV1/yolo/yolo_akidanet_widerface_iq8_wq4_aq4.h5
+wget https://data.brainchip.com/models/AkidaV1/pointnet_plus/pointnet_plus_modelnet40_iq8_wq4_aq4.h5
+wget https://data.brainchip.com/models/AkidaV1/akidanet_edge/akidanet_faceidentification_edge_iq8_wq4_aq4.h5
+wget https://data.brainchip.com/models/AkidaV1/ds_cnn/ds_cnn_kws_iq8_wq4_aq4_laq1.h5
 
 find . -maxdepth 1 -type f \( -name "*.h5" \) -exec cnn2snn convert -m  {} \;
 rm *.h5
 
 
 # Get datasets for examples
+```
 cd ..
 cd datasets
 
@@ -19,3 +22,4 @@ wget https://data.brainchip.com/dataset-mirror/voc/test_20_classes.tfrecord
 wget https://data.brainchip.com/dataset-mirror/jester/jester_subset.tar.gz
 tar -xzf ./jester_subset.tar.gz  jester_subset/jester-v1-labels.csv 
 rm ./jester_subset.tar.gz
+```
